31d93a9527517ee3e36e1d0015ae86f2bc5cc928
Operatorliquid <josestratta4@gmail.com>
2026-02-19 02:44:04 -0300

    s

--- packages/integrations/src/whatsapp/evolution.client.ts
+++ packages/integrations/src/whatsapp/evolution.client.ts
@@ -1,5 +1,5 @@
 /**
- * Evolution API (v2) WhatsApp Client
+ Evolution API (v2) WhatsApp Client
  * Sends WhatsApp messages through an Evolution API server (Baileys / WhatsApp Business engine).
  *
  * Docs: https://doc.evolution-api.com/v2/en/get-started/introduction
